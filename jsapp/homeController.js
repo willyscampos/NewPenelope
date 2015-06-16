b5ed8f945f81147793fb14e22ce3c72df0d51816
willyscampos <willyscampos@gmail.com>
2015-06-16 08:39:59 -0300

    lets go
    lets go

---
+++ jsapp/homeController.js
@@ -0,0 +1,7 @@
+﻿app.controller('homeController', function ($rootScope, $scope, $http, $location) {
+
+    if ($rootScope.idUsuario == 0) //usuário já está logado
+    {
+        $location.path('/');
+    }
+});
